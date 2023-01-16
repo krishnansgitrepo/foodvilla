1eb5d05d113b1e00b5087a2f7b1763dad0ab17c7
vijaykrishna ananthraman <vananthraman@expedia.com>
2023-01-15 23:41:20 -0800

    chap 5 assignment

--- body.js
+++ body.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IMG_CDN_URL } from "./constants";
 import { restaurantList } from "./data";
 
@@ -30,22 +31,65 @@ const RestaurantCardWithDestructuring = function ({restaurant}) {
 };
 
 //props - passing arguments to functional component
+//react uses oneway databinding
+
+//what is state
+
+//why do i need state variables - react ONLY tracks state variables and updates dom
+
+
+//what is hooks => normal function nothing fancy
+
+//what is useState => fn to create state variables, comes from react lib
+//returns an array
+//first element of array is variable name,  
+//second element is fn to update variable
+
+
 const BodyComponent = function () {
-    let searchTxt = "KFC";
+    
+    //using useState hook
+    const [searchInput, setSearchInput] = useState("Namaste");//local state variable in react 
+
+    const [allrestaurants, setAllRestaurants] = useState(restaurantList);//initially restaurants is loaded with entire restaurantList
+
+    const [filteredRestaurants, setFilteredRestaurants] = useState(restaurantList);
+
+    //everytime you want your variables to be in sync with UI you need to use state variables
+
+    function filterFunction(restaurantName, restaurants) {
+        console.log(restaurantName);
+        return restaurants.filter(r => r.data.name.includes(restaurantName));
+    }
+
+    
     return (
-        <>
+        <>  
             <div className="search-container">
-                <input type="text" className="search-input" placeholder="Search" value={searchTxt} onChange={(e) => console.log(e.target.value)}/>
-                <button className="search-btn">Search</button>
+                <input type="text" 
+                className="search-input" 
+                placeholder="Search" 
+                value={searchInput}
+                onChange = {(e) => {
+                    //e.target.value => whatever you write in input
+                    setSearchInput(e.target.value);//setSearchInput will update searchInput variable 
+                }}
+
+            />
+            <button className="search-btn" 
+            onClick = { () => {
+                const filRestaurants = filterFunction(searchInput, allrestaurants);
+                setFilteredRestaurants(filRestaurants);
+                //allrestaurants variable will hold the filtered list of restaurants
+            }}>Search</button>
+
             </div>
             <div className="restaurant-list">
                 {
-                    restaurantList.map(arestaurant => {
-                        console.log(arestaurant.data.avgRating);
+                    filteredRestaurants.map(arestaurant => {
                         return <RestaurantCardWithDestructuring restaurant = {arestaurant} key={arestaurant.data.id}/>
                     })
-                }
-            
+                }            
             </div>
         </>
     );
